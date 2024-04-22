d93a303bfd39fa5de14bb3534e517218d52f2c02
hlop3z <23062270+hlop3z@users.noreply.github.com>
2024-04-21 23:25:26 -0500

    getting-started

---
+++ interview-cheat-sheet/src/app.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "preact/hooks";
+import asymptoticNotations from "./fixtures/asymptotic-notations.json";
+import timeComplexities from "./fixtures/time-complexities.json";
+import sortingAlgorithms from "./fixtures/sorting-algorithms.json";
+
+// console.log(asymptoticNotations);
+// console.log(timeComplexities);
+console.log(sortingAlgorithms);
+
+export function App() {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  );
+}
---
+++ interview-cheat-sheet/src/main.tsx
@@ -0,0 +1,5 @@
+import { render } from "preact";
+import { App } from "./app.tsx";
+import "./style/index.css";
+
+render(<App />, document.getElementById("app")!);
